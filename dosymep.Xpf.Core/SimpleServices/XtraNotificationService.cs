8d7a80f9a55cd11e7a58e451da0857ef4afaabe5
dosymep <dosymep@gmail.com>
2022-03-04 11:27:31 +0300

    Добавил настройку названия уведомлений

--- dosymep.Xpf.Core/SimpleServices/XtraNotificationService.cs
+++ dosymep.Xpf.Core/SimpleServices/XtraNotificationService.cs
@@ -10,12 +10,18 @@ namespace dosymep.Xpf.Core.SimpleServices
         private readonly NotificationService _notificationService
             = new NotificationService()
             {
-                ApplicationId = "BIM4Everyone", 
+                ApplicationId = "dosymep", 
                 UseWin8NotificationsIfAvailable=true,
                 PredefinedNotificationTemplate = NotificationTemplate.ShortHeaderAndTwoTextFields,
                 PredefinedNotificationDuration = PredefinedNotificationDuration.Default,
                 Sound = PredefinedSound.Notification_Default
             };
+        
+        public XtraNotificationService() { }
+
+        public XtraNotificationService(string applicationId) {
+            _notificationService.ApplicationId = applicationId;
+        }
 
         public INotification CreateNotification(string title, string body, string footer, ImageSource imageSource = null)
         {
