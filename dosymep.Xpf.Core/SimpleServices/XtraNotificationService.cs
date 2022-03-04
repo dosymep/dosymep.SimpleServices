c93f5ff8540850414388a4669349c04d581e5efc
dosymep <dosymep@gmail.com>
2022-03-04 11:31:53 +0300

    Перенес класс уведомлений

--- dosymep.Xpf.Core/SimpleServices/XtraNotificationService.cs
+++ dosymep.Xpf.Core/SimpleServices/XtraNotificationService.cs
@@ -1,7 +1,13 @@
-﻿using System.Windows.Media;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+using DevExpress.Mvvm;
 using DevExpress.Mvvm.UI;
 
-using dosymep.SimpleServices;
+using INotification = dosymep.SimpleServices.INotification;
+using INotificationService = dosymep.SimpleServices.INotificationService;
 
 namespace dosymep.Xpf.Core.SimpleServices
 {
@@ -27,6 +33,50 @@ namespace dosymep.Xpf.Core.SimpleServices
         {
             var notification = _notificationService.CreatePredefinedNotification(title, body, footer, imageSource);
             return new XtraNotification(notification);
+        }
+        
+        private class XtraNotification : INotification
+        {
+            private readonly DevExpress.Mvvm.INotification _notification;
+
+            public XtraNotification(DevExpress.Mvvm.INotification notification)
+            {
+                _notification = notification;
+            }
+
+            public void Hide()
+            {
+                _notification.Hide();
+            }
+
+            public async Task<bool?> ShowAsync()
+            {
+                var result = await _notification.ShowAsync();
+                if (result == NotificationResult.UserCanceled)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            public Task<bool?> ShowAsync(int millisecond)
+            {
+                return ShowAsync(TimeSpan.FromMilliseconds(millisecond));
+            }
+
+            public async Task<bool?> ShowAsync(TimeSpan interval)
+            {
+                var timer = new DispatcherTimer();
+                timer.Interval = interval;
+                timer.Tick += (s, e) => Hide();
+                timer.Start();
+            
+                var result = await ShowAsync();
+                timer.Stop();
+
+                return result;
+            }
         }
     }
 }
