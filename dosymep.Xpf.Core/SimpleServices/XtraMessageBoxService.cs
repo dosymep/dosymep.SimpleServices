c6b39b3be9fb4491e479792859f2f888efc53edd
dosymep <dosymep@gmail.com>
2022-03-10 09:57:21 +0300

    Добавил настройки при инициализации

--- dosymep.Xpf.Core/SimpleServices/XtraMessageBoxService.cs
+++ dosymep.Xpf.Core/SimpleServices/XtraMessageBoxService.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Windows;
 using DevExpress.Mvvm;
+using DevExpress.Pdf;
 using DevExpress.Xpf.Core;
 
 namespace dosymep.Xpf.Core.SimpleServices
 {
-    public class XtraMessageBoxService : dosymep.SimpleServices.IMessageBoxService
-    {
-        private readonly DXMessageBoxService _messageBoxService = new DXMessageBoxService();
-
+    public class XtraMessageBoxService : dosymep.SimpleServices.IMessageBoxService {
+        private readonly DXMessageBoxService _messageBoxService = new DXMessageBoxService() {
+            SetMessageBoxOwner = true
+        };
+        
         public MessageBoxResult Show(string messageBoxText, string caption,
             MessageBoxButton button, MessageBoxImage icon, MessageBoxResult defaultResult) {
             return _messageBoxService.Show(messageBoxText, caption, button, icon, defaultResult);
