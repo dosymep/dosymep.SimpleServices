5ca644bece018ef3db7b0b40fca985f32d6844d7
budaevaler <valeria.budaeva@yandex.ru>
2022-04-01 18:11:52 +0300

    Добавлен интерфейс и реализация для сервиса выбора языка

---
+++ dosymep.SimpleServices/ILanguageService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dosymep.SimpleServices {
+    /// <summary>
+    /// Предоставляет доступ к языку интерфейса.
+    /// </summary>
+    public interface ILanguageService {
+
+        /// <summary>
+        /// Используемый язык.
+        /// </summary>
+        Language HostLanguage { get; }
+    }
+
+    /// <summary>
+    /// Используемые языки.
+    /// </summary>
+    public enum Language {
+        /// <summary>
+        /// Английский.
+        /// </summary>
+        EnglishUSA,
+        /// <summary>
+        /// Русский.
+        /// </summary>
+        Russian
+    }
+}
---
+++ dosymep.Xpf.Core/SimpleServices/XtraWindowsLanguageService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.SimpleServices;
+
+namespace dosymep.Xpf.Core.SimpleServices {
+    /// <summary>
+    /// Предоставляет доступ к языку Windows.
+    /// </summary>
+    public class XtraWindowsLanguageService : ILanguageService { 
+        /// <inheritdoc/>
+        public Language HostLanguage => GetWindowLanguage();
+
+        private Language GetWindowLanguage() {
+            CultureInfo ci = CultureInfo.InstalledUICulture;
+            if(ci.Name.Equals("en-US", StringComparison.CurrentCultureIgnoreCase))
+                return Language.EnglishUSA;
+            return Language.Russian;
+        }
+    }
+}
