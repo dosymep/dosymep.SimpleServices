534934d5cab5aa73916b81f5fd0018a5603c7056
budaevaler <valeria.budaeva@yandex.ru>
2022-04-04 12:14:03 +0300

    Добавлены языки в XtraWindowsLanguageService

--- dosymep.Xpf.Core/SimpleServices/XtraWindowsLanguageService.cs
+++ dosymep.Xpf.Core/SimpleServices/XtraWindowsLanguageService.cs
@@ -17,9 +17,42 @@ namespace dosymep.Xpf.Core.SimpleServices {
 
         private Language GetWindowLanguage() {
             CultureInfo ci = CultureInfo.InstalledUICulture;
-            if(ci.Name.Equals("en-US", StringComparison.CurrentCultureIgnoreCase))
+            switch(ci.Name) {
+                case "en-US":
                 return Language.EnglishUSA;
-            return Language.Russian;
+                case "ru-RU":
+                return Language.Russian;
+                case "de-DE":
+                return Language.German;
+                case "es-ES":
+                return Language.Spanish;
+                case "fr-FR":
+                return Language.French;
+                case "it-IT":
+                return Language.Italian;
+                case "nl-NL":
+                return Language.Dutch;
+                case "zh-CHS":
+                return Language.ChineseSimplified;
+                case "zh-CHT":
+                return Language.ChineseTraditional;
+                case "ja-JP":
+                return Language.Japanese;
+                case "ko-KR":
+                return Language.Korean;
+                case "cs-CZ":
+                return Language.Czech;
+                case "pl-PL":
+                return Language.Polish;
+                case "hu-HU":
+                return Language.Hungarian;
+                case "pt-BR":
+                return Language.BrazilianPortuguese;
+                case "en-GB":
+                return Language.EnglishGB;
+                default:
+                return Language.EnglishUSA;
+            }
         }
     }
 }
