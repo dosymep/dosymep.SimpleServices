d137da210b89624157bdc7968d43de3fd43353f7
dosymep <dosymep@gmail.com>
2022-05-25 11:20:05 +0300

    Добавил сервис сериализации

---
+++ dosymep.SimpleServices/ISerializationService.cs
@@ -0,0 +1,22 @@
+﻿namespace dosymep.SimpleServices {
+    /// <summary>
+    /// Сервис сериализации.
+    /// </summary>
+    public interface ISerializationService {
+        /// <summary>
+        /// Сериализует объект в строку.
+        /// </summary>
+        /// <typeparam name="T">Тип объекта сериализации.</typeparam>
+        /// <param name="object">Экземпляр объекта сериализации.</param>
+        /// <returns>Возвращает строковое значение сериализованного объекта.</returns>
+        string Serialize<T>(T @object);
+
+        /// <summary>
+        /// Десериализует строку в объект.
+        /// </summary>
+        /// <typeparam name="T">Тип результата десериализации.</typeparam>
+        /// <param name="text">Строка десериализуемого объекта.</param>
+        /// <returns>Возвращает экземпляр десериализованного объекта.</returns>
+        T Deserialize<T>(string text);
+    }
+}
