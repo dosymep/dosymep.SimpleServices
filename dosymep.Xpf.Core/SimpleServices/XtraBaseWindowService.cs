f484160040b0d92d560f74b4bfa8424c7840a78f
dosymep <dosymep@gmail.com>
2022-05-27 10:10:46 +0300

    Добавил родительский сервис для окна

---
+++ dosymep.Xpf.Core/SimpleServices/XtraBaseWindowService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+
+using DevExpress.Mvvm.UI;
+using DevExpress.Mvvm.UI.Interactivity;
+using DevExpress.Xpf.Grid;
+
+namespace dosymep.Xpf.Core.SimpleServices {
+    /// <summary>
+    /// Абстрактный класс сервиса окна.
+    /// </summary>
+    public abstract class XtraBaseWindowService<TServiceBase> : IDisposable
+        where TServiceBase : ServiceBase {
+        
+        /// <summary>
+        /// Родительское окно сервиса
+        /// </summary>
+        protected readonly Window _window;
+
+        /// <summary>
+        /// Привязанный сервис к окну.
+        /// </summary>
+        protected readonly TServiceBase _serviceBase;
+
+        /// <summary>
+        /// Создает экземпляр сервиса окна.
+        /// </summary>
+        /// <param name="window">Родительское окно для сервиса.</param>
+        /// <param name="serviceBase">Привязываемый сервис к окну.</param>
+        protected XtraBaseWindowService(Window window, TServiceBase serviceBase) {
+            _window = window;
+            _serviceBase = serviceBase;
+
+            Interaction.GetBehaviors(_window).Add(_serviceBase);
+        }
+
+        /// <summary>
+        /// Очищает привязку к окну.
+        /// </summary>
+        /// <param name="disposing">Указывает на очистку ресурсов.</param>
+        protected virtual void Dispose(bool disposing) {
+            if(disposing) {
+                Interaction.GetBehaviors(_window).Remove(_serviceBase);
+            }
+        }
+
+        /// <inheritdoc />
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
