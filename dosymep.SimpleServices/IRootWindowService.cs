3e2501e2112874b0dfa04f37dd86271fb8109bec
dosymep <dosymep@gmail.com>
2022-06-01 10:01:20 +0300

    Добавил интерфейс корневого окна.

---
+++ dosymep.SimpleServices/IRootWindowService.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace dosymep.SimpleServices {
+    /// <summary>
+    /// Интерфейс рабочего окна
+    /// </summary>
+    public interface IRootWindowService {
+        /// <summary>
+        /// Текущее корневое окно.
+        /// </summary>
+        Window RootWindow { get; set; }
+    }
+}
---
+++ dosymep.SimpleServices/RootWindowService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace dosymep.SimpleServices {
+    /// <summary>
+    /// Класс текущего рабочего окна.
+    /// </summary>
+    public class RootWindowService : IRootWindowService {
+        private Window _rootWindow;
+
+        /// <inheritdoc />
+        public Window RootWindow {
+            get => _rootWindow;
+            set {
+                _rootWindow = value;
+                if(_rootWindow != null) {
+                    _rootWindow.Closed += RootWindowOnClosed;
+                }
+            }
+        }
+
+        private void RootWindowOnClosed(object sender, EventArgs e) {
+            _rootWindow.Closed -= RootWindowOnClosed;
+            _rootWindow = null;
+        }
+    }
+}
