85127c097da91b2f0a2d7280f8e06f5f4729ebc8
dosymep <dosymep@gmail.com>
2022-06-01 12:29:48 +0300

    Добавил сервис по обновлению темы у окна

---
+++ dosymep.SimpleServices/IThemeUpdaterService.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace dosymep.SimpleServices {
+    /// <summary>
+    /// Интерфейс по обновлению тем окон.
+    /// </summary>
+    public interface IThemeUpdaterService {
+        /// <summary>
+        /// Устанавливает текущую тему окна.
+        /// </summary>
+        /// <param name="window">Окно, которому устанавливают тему.</param>
+        /// <param name="theme">Устанавливаемая тема окна.</param>
+        void SetTheme(Window window, UIThemes theme);
+    }
+}
---
+++ dosymep.Xpf.Core/SimpleServices/XtraThemeUpdaterService.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+using DevExpress.Xpf.Core;
+
+using dosymep.SimpleServices;
+
+namespace dosymep.Xpf.Core.SimpleServices {
+    /// <summary>
+    /// Класс по обновлению тем окон.
+    /// </summary>
+    public class XtraThemeUpdaterService : IThemeUpdaterService {
+        /// <inheritdoc />
+        public void SetTheme(Window window, UIThemes theme) {
+            if(theme == UIThemes.Dark) {
+                ThemeManager.SetThemeName(window, Theme.Win10DarkName);
+            } else if(theme == UIThemes.Light) {
+                ThemeManager.SetThemeName(window, Theme.Win10LightName);
+            }
+        }
+    }
+}
