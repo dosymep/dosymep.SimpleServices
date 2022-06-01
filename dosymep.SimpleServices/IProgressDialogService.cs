e9bfe798c26b4394af6158801b62bdfd11d4b88c
dosymep <dosymep@gmail.com>
2022-06-01 09:06:20 +0300

    Добавил интерфейс сервиса диалога прогресса

---
+++ dosymep.SimpleServices/IProgressDialogService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace dosymep.SimpleServices {
+    /// <summary>
+    /// Интерфейс прогресс диалога.
+    /// </summary>
+    public interface IProgressDialogService : IDisposable {
+        /// <summary>
+        /// Максимальное значение прогресса.
+        /// </summary>
+        int MaxValue { get; set; }
+
+        /// <summary>
+        /// Шаг обновления значение прогресса.
+        /// </summary>
+        int StepValue { get; set; }
+
+        /// <summary>
+        /// Строка форматирования отображения.
+        /// </summary>
+        string DisplayTitleFormat { get; set; }
+
+        /// <summary>
+        /// Создает класс прогресса.
+        /// </summary>
+        /// <returns>Возвращает прогресс.</returns>
+        IProgress<int> CreateProgress();
+
+        /// <summary>
+        /// Создает класс прогресса.
+        /// </summary>
+        /// <returns>Возвращает прогресс.</returns>
+        IProgress<int> CreateAsyncProgress();
+
+        /// <summary>
+        /// Возвращает токен отмены.
+        /// </summary>
+        /// <returns>Возвращает токен отмены.</returns>
+        CancellationToken CreateCancellationToken();
+    }
+}
