75e2b90efb2b47ae73f534389e8079381e388e33
dosymep <dosymep@gmail.com>
2022-11-08 08:16:23 +0300

    Переделал интерфейс сервиса языка

--- dosymep.SimpleServices/ILanguageService.cs
+++ dosymep.SimpleServices/ILanguageService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,80 +10,9 @@ namespace dosymep.SimpleServices {
     /// Предоставляет доступ к языку интерфейса.
     /// </summary>
     public interface ILanguageService {
-
         /// <summary>
         /// Используемый язык.
         /// </summary>
-        Language HostLanguage { get; }
-    }
-
-    /// <summary>
-    /// Используемые языки.
-    /// </summary>
-    public enum Language {
-        /// <summary>
-        /// Английский США.
-        /// </summary>
-        EnglishUSA,
-        /// <summary>
-        /// Русский.
-        /// </summary>
-        Russian,
-        /// <summary>
-        /// Немецкий.
-        /// </summary>
-        German,
-        /// <summary>
-        /// Испанский.
-        /// </summary>
-        Spanish,
-        /// <summary>
-        /// Французский.
-        /// </summary>
-        French,
-        /// <summary>
-        /// Итальянский.
-        /// </summary>
-        Italian,
-        /// <summary>
-        /// Голландский.
-        /// </summary>
-        Dutch,
-        /// <summary>
-        /// Упрощенный китайский.
-        /// </summary>
-        ChineseSimplified,
-        /// <summary>
-        /// Традиционный китайский.
-        /// </summary>
-        ChineseTraditional,
-        /// <summary>
-        /// Японский.
-        /// </summary>
-        Japanese,
-        /// <summary>
-        /// Корейский.
-        /// </summary>
-        Korean,
-        /// <summary>
-        /// Чешский.
-        /// </summary>
-        Czech,
-        /// <summary>
-        /// Польский.
-        /// </summary>
-        Polish,
-        /// <summary>
-        /// Венгерский.
-        /// </summary>
-        Hungarian,
-        /// <summary>
-        /// Португальский.
-        /// </summary>
-        BrazilianPortuguese,
-        /// <summary>
-        /// Английский Великобритания.
-        /// </summary>
-        EnglishGB,
+        CultureInfo HostLanguage { get; }
     }
 }
--- dosymep.Xpf.Core/SimpleServices/XtraWindowsLanguageService.cs
+++ dosymep.Xpf.Core/SimpleServices/XtraWindowsLanguageService.cs
@@ -11,48 +11,8 @@ namespace dosymep.Xpf.Core.SimpleServices {
     /// <summary>
     /// Класс сервиса доступа к языку Windows.
     /// </summary>
-    public class XtraWindowsLanguageService : ILanguageService { 
+    public class XtraWindowsLanguageService : ILanguageService {
         /// <inheritdoc/>
-        public Language HostLanguage => GetWindowLanguage();
-
-        private Language GetWindowLanguage() {
-            CultureInfo ci = CultureInfo.InstalledUICulture;
-            switch(ci.Name) {
-                case "en-US":
-                return Language.EnglishUSA;
-                case "ru-RU":
-                return Language.Russian;
-                case "de-DE":
-                return Language.German;
-                case "es-ES":
-                return Language.Spanish;
-                case "fr-FR":
-                return Language.French;
-                case "it-IT":
-                return Language.Italian;
-                case "nl-NL":
-                return Language.Dutch;
-                case "zh-CHS":
-                return Language.ChineseSimplified;
-                case "zh-CHT":
-                return Language.ChineseTraditional;
-                case "ja-JP":
-                return Language.Japanese;
-                case "ko-KR":
-                return Language.Korean;
-                case "cs-CZ":
-                return Language.Czech;
-                case "pl-PL":
-                return Language.Polish;
-                case "hu-HU":
-                return Language.Hungarian;
-                case "pt-BR":
-                return Language.BrazilianPortuguese;
-                case "en-GB":
-                return Language.EnglishGB;
-                default:
-                return Language.EnglishUSA;
-            }
-        }
+        public CultureInfo HostLanguage => CultureInfo.InstalledUICulture;
     }
 }
