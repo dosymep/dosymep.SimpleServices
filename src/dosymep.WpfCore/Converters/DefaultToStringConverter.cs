0d9bce346b1f070a46ae02278ef507b74c77b586
dosymep <dosymep@gmail.com>
2025-02-26 13:29:47 +0300

    Изменил название конвертера и поправил его

--- src/dosymep.WpfCore/Converters/DefaultToStringConverter.cs
+++ src/dosymep.WpfCore/Converters/DefaultToStringConverter.cs
@@ -8,27 +8,26 @@ namespace dosymep.WpfCore.Converters;
 /// </summary>
 [ValueConversion(typeof(object), typeof(string))]
 [ValueConversion(typeof(string), typeof(string))]
-public sealed class NullToStringConverter : IValueConverter {
+public sealed class DefaultToStringConverter : IValueConverter {
     /// <summary>
     /// Конструирует объект.
     /// </summary>
-    public NullToStringConverter() { }
+    public DefaultToStringConverter() { }
 
     /// <summary>
     /// Конструирует объект.
     /// </summary>
-    /// <param name="nullValue">Значение, которое будет отображаться если null.</param>
-    public NullToStringConverter(string nullValue) => NullValue = nullValue;
+    /// <param name="defaultValue">Значение, которое будет отображаться если null.</param>
+    public DefaultToStringConverter(string defaultValue) => DefaultValue = defaultValue;
 
     /// <summary> 
     /// Вывод, когда значение Null.
     /// </summary>
-    public string NullValue { get; set; } = "<Нет данных>";
+    public string DefaultValue { get; set; } = "<Нет данных>";
 
     /// <inheritdoc />
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-        string? stringValue = value?.ToString();
-        return string.IsNullOrEmpty(stringValue) ? NullValue : value;
+        return ReferenceEquals(value, default) ? DefaultValue : value;
     }
 
     /// <inheritdoc />
