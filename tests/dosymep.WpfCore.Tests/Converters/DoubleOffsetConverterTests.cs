ea4ad28b099f78b911a216e764fb8263d284fac5
dosymep <dosymep@gmail.com>
2025-02-27 16:20:20 +0300

    Обновил тесты

--- tests/dosymep.WpfCore.Tests/Converters/DoubleOffsetConverterTests.cs
+++ tests/dosymep.WpfCore.Tests/Converters/DoubleOffsetConverterTests.cs
@@ -6,15 +6,21 @@ namespace dosymep.WpfCore.Tests.Converters;
 
 public sealed class DoubleOffsetConverterTests {
     private readonly DoubleOffsetConverter _valueConverter;
+    private readonly DoubleOffsetConverter _invertedValueConverter;
 
     public DoubleOffsetConverterTests() {
         double offsetValue = 42.0;
         _valueConverter = new DoubleOffsetConverter(offsetValue);
+        _invertedValueConverter = new DoubleOffsetConverter(true, offsetValue);
     }
 
     [Fact]
     public void Convert_Default_ReturnsDefault() {
-        Assert.Equal(default, _valueConverter.Convert(default, typeof(double), default, CultureInfo.CurrentCulture));
+        Assert.Equal(default, 
+            _valueConverter.Convert(default, typeof(double), default, CultureInfo.CurrentCulture));
+        
+        Assert.Equal(default, 
+            _invertedValueConverter.ConvertBack(default, typeof(double), default, CultureInfo.CurrentCulture));
     }
 
     [Fact]
@@ -22,6 +28,9 @@ public sealed class DoubleOffsetConverterTests {
         double value = 73.0;
         Assert.Equal(value + _valueConverter.OffsetValue,
             _valueConverter.Convert(value, typeof(double), default, CultureInfo.CurrentCulture));
+        
+        Assert.Equal(value + _invertedValueConverter.OffsetValue,
+            _invertedValueConverter.ConvertBack(value, typeof(double), default, CultureInfo.CurrentCulture));
     }
 
     [Fact]
@@ -29,5 +38,8 @@ public sealed class DoubleOffsetConverterTests {
         double value = 73.0;
         Assert.Equal(value - _valueConverter.OffsetValue,
             _valueConverter.ConvertBack(value, typeof(double), default, CultureInfo.CurrentCulture));
+        
+        Assert.Equal(value - _invertedValueConverter.OffsetValue,
+            _invertedValueConverter.Convert(value, typeof(double), default, CultureInfo.CurrentCulture));
     }
 }
--- tests/dosymep.WpfCore.Tests/Converters/IntOffsetConverterTests.cs
+++ tests/dosymep.WpfCore.Tests/Converters/IntOffsetConverterTests.cs
@@ -6,15 +6,21 @@ namespace dosymep.WpfCore.Tests.Converters;
 
 public sealed class IntOffsetConverterTests {
     private readonly IntOffsetConverter _valueConverter;
+    private readonly IntOffsetConverter _invertedValueConverter;
 
     public IntOffsetConverterTests() {
         int offsetValue = 42;
         _valueConverter = new IntOffsetConverter(offsetValue);
+        _invertedValueConverter = new IntOffsetConverter(true, offsetValue);
     }
 
     [Fact]
     public void Convert_Default_ReturnsDefault() {
-        Assert.Equal(default, _valueConverter.Convert(default, typeof(int), default, CultureInfo.CurrentCulture));
+        Assert.Equal(default, 
+            _valueConverter.Convert(default, typeof(int), default, CultureInfo.CurrentCulture));
+        
+        Assert.Equal(default,
+            _invertedValueConverter.ConvertBack(default, typeof(int), default, CultureInfo.CurrentCulture));
     }
 
     [Fact]
@@ -22,6 +28,9 @@ public sealed class IntOffsetConverterTests {
         int value = 73;
         Assert.Equal(value + _valueConverter.OffsetValue,
             _valueConverter.Convert(value, typeof(int), default, CultureInfo.CurrentCulture));
+
+        Assert.Equal(value + _invertedValueConverter.OffsetValue,
+            _invertedValueConverter.ConvertBack(value, typeof(int), default, CultureInfo.CurrentCulture));
     }
 
     [Fact]
@@ -29,5 +38,8 @@ public sealed class IntOffsetConverterTests {
         int value = 73;
         Assert.Equal(value - _valueConverter.OffsetValue,
             _valueConverter.ConvertBack(value, typeof(int), default, CultureInfo.CurrentCulture));
+
+        Assert.Equal(value - _invertedValueConverter.OffsetValue,
+            _invertedValueConverter.Convert(value, typeof(int), default, CultureInfo.CurrentCulture));
     }
 }
