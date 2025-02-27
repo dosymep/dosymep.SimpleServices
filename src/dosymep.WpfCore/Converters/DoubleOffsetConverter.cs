64bcf491780d95e82d0a83ae08d2fb423ae3d80e
dosymep <dosymep@gmail.com>
2025-02-27 16:12:09 +0300

    Добавил инверсию для offset конвертеров

--- src/dosymep.WpfCore/Converters/DoubleOffsetConverter.cs
+++ src/dosymep.WpfCore/Converters/DoubleOffsetConverter.cs
@@ -12,13 +12,28 @@ public sealed class DoubleOffsetConverter : IValueConverter {
     /// Конструирует объект.
     /// </summary>
     public DoubleOffsetConverter() { }
-
+    
     /// <summary>
     /// Конструирует объект.
     /// </summary>
     /// <param name="offsetValue">Значение на которое изменяется.</param>
     public DoubleOffsetConverter(double offsetValue) => OffsetValue = offsetValue;
-    
+
+    /// <summary>
+    /// Конструирует объект.
+    /// </summary>
+    /// <param name="inverted">Признак инвертированости.</param>
+    /// <param name="offsetValue">Значение на которое изменяется.</param>
+    public DoubleOffsetConverter(bool inverted, double offsetValue) {
+        Inverted = inverted;
+        OffsetValue = offsetValue;
+    }
+
+    /// <summary>
+    /// Признак инвертированости.
+    /// </summary>
+    public bool Inverted { get; set; }
+
     /// <summary>
     /// Значение на которое изменяется.
     /// </summary>
@@ -26,11 +41,19 @@ public sealed class DoubleOffsetConverter : IValueConverter {
 
     /// <inheritdoc />
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-        return value is double doubleValue ? doubleValue + OffsetValue : value;
+        return !Inverted ? Add(value) : Subtract(value);
     }
 
     /// <inheritdoc />
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        return Inverted ? Add(value) : Subtract(value);
+    }
+
+    private object? Add(object? value) {
+        return value is double doubleValue ? doubleValue + OffsetValue : value;
+    }
+
+    private object? Subtract(object? value) {
         return value is double doubleValue ? doubleValue - OffsetValue : value;
     }
 }
--- src/dosymep.WpfCore/Converters/IntOffsetConverter.cs
+++ src/dosymep.WpfCore/Converters/IntOffsetConverter.cs
@@ -12,13 +12,28 @@ public sealed class IntOffsetConverter : IValueConverter {
     /// Конструирует объект.
     /// </summary>
     public IntOffsetConverter() { }
-
+    
     /// <summary>
     /// Конструирует объект.
     /// </summary>
     /// <param name="offsetValue">Значение на которое изменяется.</param>
     public IntOffsetConverter(int offsetValue) => OffsetValue = offsetValue;
-    
+
+    /// <summary>
+    /// Конструирует объект.
+    /// </summary>
+    /// <param name="inverted">Признак инвертированости.</param>
+    /// <param name="offsetValue">Значение на которое изменяется.</param>
+    public IntOffsetConverter(bool inverted, int offsetValue) {
+        Inverted = inverted;
+        OffsetValue = offsetValue;
+    }
+
+    /// <summary>
+    /// Признак инвертированости.
+    /// </summary>
+    public bool Inverted { get; set; }
+
     /// <summary>
     /// Значение на которое изменяется.
     /// </summary>
@@ -26,11 +41,19 @@ public sealed class IntOffsetConverter : IValueConverter {
 
     /// <inheritdoc />
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-        return value is int intValue ? intValue + OffsetValue : value;
+        return !Inverted ? Add(value) : Subtract(value);
     }
 
     /// <inheritdoc />
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        return Inverted ? Add(value) : Subtract(value);
+    }
+
+    private object? Add(object? value) {
+        return value is int intValue ? intValue + OffsetValue : value;
+    }
+
+    private object? Subtract(object? value) {
         return value is int intValue ? intValue - OffsetValue : value;
     }
 }
