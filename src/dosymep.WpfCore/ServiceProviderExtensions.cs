663103e7ebd36e128887cfad16be9fcb4d6f854a
dosymep <dosymep@gmail.com>
2025-02-27 14:05:20 +0300

    Добавил документацию

--- src/dosymep.WpfCore/ServiceProviderExtensions.cs
+++ src/dosymep.WpfCore/ServiceProviderExtensions.cs
@@ -4,7 +4,16 @@ using dosymep.SimpleServices;
 
 namespace dosymep.WpfCore;
 
+/// <summary>
+/// Расширения интерфейса <see cref="IServiceProvider"/>.
+/// </summary>
 public static class ServiceProviderExtensions {
+    /// <summary>
+    /// Возвращает root объект и приводит к заданому типу.
+    /// </summary>
+    /// <param name="serviceProvider">Интерфейс провайдера сервисов.</param>
+    /// <typeparam name="T">Тип в который нужно привести root объект.</typeparam>
+    /// <returns>Возвращает root объект сконвертированный в нужный тип.</returns>
     public static T? GetRootObject<T>(this IServiceProvider serviceProvider) where T : class {
         IRootObjectProvider rootObjectProvider =
             (IRootObjectProvider) serviceProvider.GetService(typeof(IRootObjectProvider));
